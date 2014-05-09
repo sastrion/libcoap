2939a667633765aee045d6e7ea75f0e9e157e322
Maciej Wasilak <wasilak@gmail.com>
2014-05-09 21:06:57 +0200

    Add libcoap.mk makefile

---
+++ libcoap.mk
@@ -0,0 +1,17 @@
+# List of all the board related files.
+EXTSRC += ext/libcoap/pdu.c \
+         ext/libcoap/net.c \
+         ext/libcoap/debug.c \
+         ext/libcoap/encode.c \
+         ext/libcoap/uri.c \
+         ext/libcoap/coap_list.c \
+         ext/libcoap/resource.c \
+         ext/libcoap/hashkey.c \
+         ext/libcoap/str.c \
+         ext/libcoap/option.c \
+         ext/libcoap/async.c \
+         ext/libcoap/subscribe.c \
+         ext/libcoap/block.c
+
+# Required include directories
+EXTINC += ext/libcoap
